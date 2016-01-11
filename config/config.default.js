296b8ae2deecec18dca2cd99360a2d13a707c382
Jeremy Kay <jeremykaybass@gmail.com>
2016-01-11 15:35:35 -0500

    add placeholder text for skus array

--- config/config.default.js
+++ config/config.default.js
@@ -4,5 +4,5 @@ var appConfig = {
   app_key: '<APPKEY>',
   endpoint: "https://api.zype.com/",
   player_endpoint: "https://player.zype.com/",
-  subscriptionSkus: []
+  subscriptionSkus: <SKUS_ARRAY>
 };
