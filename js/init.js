a14599c0fb21e41e8a8c8d97715254b0da9513ce
Khurshid Fayzullaev <khurshidfayzullaev@yahoo.com>
2016-01-16 19:08:20 -0500

    Temporarily do not apply design properties received from the API. Use a custom one instead.

--- js/init.js
+++ js/init.js
@@ -41,18 +41,18 @@
             settings.displayButtons = true;
           }
 
-          // main colors
-          settings.backgroundColor = app_json.response.background_color;
-          settings.topBarColor = app_json.response.top_bar_color;
-          settings.textColor = app_json.response.text_color;
-          settings.mutedTextColor =app_json.response.muted_text_color;
-          settings.brandColor = app_json.response.brand_color;
-
-          // navigation colors
-          settings.leftNavBackgroundColor = app_json.response.left_nav_background_color;
-          settings.leftNavTextColor = app_json.response.left_nav_text_color;
-          settings.leftNavHoverTextColor = app_json.response.left_nav_hover_text_color;
-          settings.leftNavHoverBackgroundColor = app_json.response.left_nav_hover_background_color;
+          // // main colors
+          // settings.backgroundColor = app_json.response.background_color;
+          // settings.topBarColor = app_json.response.top_bar_color;
+          // settings.textColor = app_json.response.text_color;
+          // settings.mutedTextColor =app_json.response.muted_text_color;
+          // settings.brandColor = app_json.response.brand_color;
+          //
+          // // navigation colors
+          // settings.leftNavBackgroundColor = app_json.response.left_nav_background_color;
+          // settings.leftNavTextColor = app_json.response.left_nav_text_color;
+          // settings.leftNavHoverTextColor = app_json.response.left_nav_hover_text_color;
+          // settings.leftNavHoverBackgroundColor = app_json.response.left_nav_hover_background_color;
 
           // icon
           settings.icon = utils.makeSSL(app_json.response.logo_original_url);
