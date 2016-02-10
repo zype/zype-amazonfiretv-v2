b9d21ac3163c575627665ceed090a38763af79d3
Jeremy Kay <jeremykaybass@gmail.com>
2016-02-10 11:55:06 -0500

    shouldn't need defaults from these

--- js/video-model.js
+++ js/video-model.js
@@ -4,17 +4,17 @@
  */
 
 var Video = function(args) {
-  this.id = args.id || "";
-  this.title = args.title || "";
+  this.id = args.id;
+  this.title = args.title;
   this.pubDate = args.pubDate || "";
-  this.thumbURL = utils.makeSSL(args.thumbURL) || "";
-  this.imgURL = utils.makeSSL(args.imgURL) || "";
+  this.thumbURL = utils.makeSSL(args.thumbURL);
+  this.imgURL = utils.makeSSL(args.imgURL);
   this.description = args.description || "";
   this.seconds = args.seconds || "";
-  this.subscription_required = args.subscription_required || null;
-  this.rental_required = args.rental_required || null;
-  this.purchase_required = args.purchase_required || null;
-  this.pass_required = args.pass_required || null;
+  this.subscription_required = args.subscription_required;
+  this.rental_required = args.rental_required;
+  this.purchase_required = args.purchase_required;
+  this.pass_required = args.pass_required;
   this.ad_schedule = [];
 };
 
