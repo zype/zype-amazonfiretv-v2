ba45f1715b533b5392dfb0e616b1715c04472907
Aaron Colon <aaron.colon@gmail.com>
2017-03-03 15:22:53 -0800

    update client_id and client_secret

--- config/config.default.js
+++ config/config.default.js
@@ -4,8 +4,8 @@
 
 var appConfig = {
   app_key: '<APPKEY>',
-  client_id: '<CLIENTID>',
-  client_secret: '<CLIENTSECRET>',
+  client_id: '<CLIENT_ID>',
+  client_secret: '<CLIENT_SECRET>',
   endpoint: 'https://api.zype.com/',
   player_endpoint: 'https://player.zype.com/'
 };
